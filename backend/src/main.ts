348cf1b4ed5a976d3d245085b3b898f0c3b63c56
luongtrw <luongtran@nostech.vn>
2025-11-25 01:47:02 +0700

    fix cookie parser import

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
